77ba068766f1898f5db9fd5250c7ac6e2875d361
Jurriaan Bremer <jurriaanbremer@gmail.com>
2015-09-04 23:48:53 +0200

    allow proper searching of hashes and other stuff

--- vtdl.sh
+++ vtdl.sh
@@ -3,24 +3,47 @@
 # Requires curl, wget, and jq.
 
 if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 <query..>"
+    echo "Usage: $0 [-1] <query..>"
+    echo "  -1: Do not split the query up, use it as a whole."
     exit 1
 fi
 
 # Fetch the apikey and the query.
 APIKEY="$(cat ~/.vtdl)"
-QUERY="$*"
 
 if [ -z "$APIKEY" ]; then
     echo "Please provide a proper apikey (put it in ~/.vtdl)!"
     exit 1
 fi
 
-if [ -z "$QUERY" ]; then
+if [ -z "$*" ]; then
     echo "Please provide a search query!"
     exit 1
 fi
 
-for hash in $(curl -s https://www.virustotal.com/vtapi/v2/file/search -F "apikey=$APIKEY" -F "query=$QUERY"|jq -r '.hashes[]'); do
-    echo "https://www.virustotal.com/vtapi/v2/file/download?apikey=$APIKEY&hash=$hash" -O "$hash"
-done|xargs -P 10 -n 3 wget
+# Search for something and download all matches.
+vt_search() {
+    echo "Query: $*"
+    for hash in $(curl -s https://www.virustotal.com/vtapi/v2/file/search -F "apikey=$APIKEY" -F "query=$*"|jq -r ".hashes[]"); do
+        echo "https://www.virustotal.com/vtapi/v2/file/download?apikey=$APIKEY&hash=$hash" -O "$hash"
+    done|xargs -P 10 -n 3 wget
+}
+
+if [ "$1" = "-1" ]; then
+    vt_search "$*"
+    exit 0
+fi
+
+# Try to download one or more sample(s) by hash.
+for part in $*; do
+    if echo $part|grep -Eq '^[a-fA-F0-9]{32,64}$'; then
+        if wget -q "https://www.virustotal.com/vtapi/v2/file/download?apikey=$APIKEY&hash=$part" -O "$part"; then
+            echo "Downloaded sample by hash: $part"
+        else
+            echo "Not a known hash: $part"
+            vt_search "$part"
+        fi
+    else
+        vt_search "$part"
+    fi
+done
